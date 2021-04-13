79cdd567df6f0bfca88be3899c73934770a7981b
Chris Wade <chris@wadespace.net>
2021-04-13 17:41:38 +1000

    done

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -4,10 +4,16 @@ function ExpenseItem(props) {
   // const expenseDate = new Date(2021, 2, 27);
   // const expenseTitle = "Car Insurance";
   // const expenseAmount = 294.67;
+  const month = props.date.toLocaleString("en-AU", { month: "long" });
+  const day = props.date.toLocaleString("en-AU", { day: "2-digit" });
+  const year = props.date.getFullYear();
 
   return (
     <div className="expense-item">
-      <div>{props.date.toLocaleDateString()}</div>
+      {/* <div>{props.date.toLocaleDateString()}</div> */}
+      <div>{month}</div>
+      <div>{year}</div>
+      <div>{day}</div>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">{props.amount}</div>
